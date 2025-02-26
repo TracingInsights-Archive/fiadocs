4b38a172a0d88667bb7a83007cd72d2524e7ab21
TracingInsights-Archive <153351807+TracingInsights-Archive@users.noreply.github.com>
2025-02-26 18:47:02 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -70,7 +70,7 @@ class FIADocumentHandler:
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
         }
-        url = "https://www.fia.com/documents/championships/fia-formula-one-world-championship-14/season/season-2024-2043"
+        url = "https://www.fia.com/documents/championships/fia-formula-one-world-championship-14/season/season-2025-2071"
         response = requests.get(url, headers=headers)
         soup = BeautifulSoup(response.content, "html.parser")
         documents = []
